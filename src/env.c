e1e9e42b9531e2376c6b63be12cf0573d9010d93
Gabriel <panci005@umn.edu>
2021-01-28 20:12:09 -0600

    feat: #06 implement environment printing for debugging

--- src/env.c
+++ src/env.c
@@ -13,7 +13,7 @@ void init_env()
 
 Expr_t* get_val(char *name)
 {
-    Env *curr = &env_head;
+    Env *curr = env_head.next;
     while (curr != NULL) {
         if (curr->expr && curr->name && !strcmp(name, curr->name)) {
             return curr->expr;
@@ -27,18 +27,39 @@ Expr_t* get_val(char *name)
 
 int set_val(char *name, Expr_t *expr)
 {
-    Env *curr = &env_head;
-    while (curr->next) {
+    Env *curr = env_head.next;
+    while (curr != NULL) {
         if (curr->name && !strcmp(curr->name, name)) {
-            if (curr->expr->r_type != expr->r_type) {
-                fprintf(stderr, "Unable to assign value of type %s to variable \"%s\" of type %s",
-                        r_str[curr->expr->r_type], name, r_str[expr->r_type]);
-                return 1;
-            }
-            else {
-            }
+            // if (curr->expr->r_type != expr->r_type) {
+            //     fprintf(stderr, "Unable to assign value of type %s to variable \"%s\" of type %s",
+            //             r_str[curr->expr->r_type], name, r_str[expr->r_type]);
+            //     exit(1);
+            // }
+            // else {
+                
+            // }
+            curr->expr = expr;
+            return 0;
+        }
+        else {
             curr = curr->next;
         }
     }
+    Env *new_env = (Env*) malloc(sizeof(Env));
+    new_env->name = (char*) malloc(strlen(name));
+    strcpy(new_env->name, name);
+    new_env->expr = expr;
+    new_env->next = NULL;
+    env_tail->next = new_env;
+    env_tail = new_env;
     return 0;
+}
+
+void print_env() {
+    Env *curr = env_head.next;
+    printf("Environment:\n");
+    while (curr != NULL) {
+        printf("\t%s: ", curr->name);
+        print_expr(*curr->expr);
+    }
 }
