8c62a768f0873c1920fd48069e46827ef6c63f13
Gabriel <panci005@umn.edu>
2021-02-04 20:54:37 -0600

    feat: #16 implement local scoping for evaluator. A local scope is created whenever inside a sequence.

--- src/evaluator.h
+++ src/evaluator.h
@@ -2,17 +2,20 @@
 #define __EVALUATOR_H__
 
 #include "expr.h"
+#include "env.h"
 
-Expr_t evaluate_expr(Expr_t expr);
+Expr_t evaluate_expr(Expr_t expr, Env *env);
 
-Expr_t evaluate_arith(Expr_t expr);
-Expr_t evaluate_set(Expr_t expr);
-Expr_t evaluate_id(Expr_t expr);
-Expr_t evaluate_bexpr(Expr_t expr);
-Expr_t evaluate_cond(Expr_t expr);
-Expr_t evaluate_sequence(Expr_t expr);
-Expr_t evaluate_list(Expr_t expr);
+Expr_t evaluate_arith(Expr_t expr, Env *env);
+Expr_t evaluate_set(Expr_t expr, Env *env);
+Expr_t evaluate_id(Expr_t expr, Env *env);
+Expr_t evaluate_bexpr(Expr_t expr, Env *env);
+Expr_t evaluate_cond(Expr_t expr, Env *env);
+Expr_t evaluate_sequence(Expr_t expr, Env *env);
+Expr_t evaluate_list(Expr_t expr, Env *env);
 
 char constant_to_bool(Expr_t expr);
+
+Expr_t expr_array_to_sequence(Expr_t *arr, int size);
 
 #endif
